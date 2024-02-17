27db0a587d5cb69cf98b25467bcfd8cdfddcb39e
pg2003 <113009509+pg2003@users.noreply.github.com>
2024-02-17 12:30:26 +0800

    src skeleton

---
+++ resources/views/Hobbies.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hobbies</title>
+</head>
+<body>
+<h1><button><a href="/AboutMe">About Me</a></button></h1>
+    <h1><button><a href="/Skills">Skills</a></button></h1>
+    <h1><button><a href="/Hobbies">Hobbies</a></button></h1> 
+    <h2>i like basketballs</h2>
+
+</body>
+</html>
---
+++ resources/views/Skills.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Skills</title>
+</head>
+<body>
+<h1><button><a href="/AboutMe">About Me</a></button></h1>
+    <h1><button><a href="/Skills">Skills</a></button></h1>
+    <h1><button><a href="/Hobbies">Hobbies</a></button></h1>  
+    <ul>
+
+    <h2>i am good at basketballs</h2>
+</ul>
+</body>
+</html>
